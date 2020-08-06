ec6c704a46e52f340b4db9895784dbe3047bd370
bretg96 <68873998+bretg96@users.noreply.github.com>
2020-08-06 07:35:31 -0700

    removed comments

--- Deadwood.java
+++ Deadwood.java
@@ -3,9 +3,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
-/**
- * Created by tesfaga on 5/1/19.
- */
+
 public class Deadwood {
 
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
